c3f39783b468450db455b4cf740c4084fc911410
danimuncs <izaac.muncal@student.ateneo.edu>
2024-03-11 12:34:47 +0800

    Changed the import and added views instead of the last change

--- magis_sawrapped/login/urls.py
+++ magis_sawrapped/login/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from . import views
+from .views import register, user_login
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('register/', register, name='register'),
+    path('login/', user_login, name='login'),
+    
 ]
