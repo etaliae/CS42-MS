0bf14d8dd30bf5ce8c9dcba8fd6c407cf37c3c24
Elaiza Bolislis <161827825+ebolislis@users.noreply.github.com>
2024-03-12 09:58:54 +0800

    Created a profs to pick app and added professor and review as models

---
+++ magis_sawrapped/profs_to_pick/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from schedo.models import Department, Subject
+
+class Professor(models.Model)
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
+    last_name = models.CharField(max_length=150)
+    given_name = models.CharField(max_length=150)
+    middle_initial = models.CharField(max_length=5, blank=True, default='')
+
+    class Meta:
+        constraints = [models.UniqueConstraint(
+            fields=['last_name', 'given_name', 'middle_initial'],
+            name='prof_uniq')]
+        ordering = ['last_name']
+
+    def __str__(self):
+        return '{}, {} {}'.format(self.last_name, self.given_name, self.middle_initial)
+
+
+
+class Review(models.Model)
+    professor = models.ForeignKey(Professor, on_delete=models.CASCADE)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    review = models.TextField()
+
+    def __str__(self):
+        return '{}'.format(self.review)
+
+
