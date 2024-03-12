72992efacbbcde98b315704aae607e2ca0fcd3c7
Elaiza Bolislis <161827825+ebolislis@users.noreply.github.com>
2024-03-12 14:23:29 +0800

    Attempted to fix the circular import error

--- magis_sawrapped/profs_to_pick/models.py
+++ magis_sawrapped/profs_to_pick/models.py
@@ -4,9 +4,10 @@ from schedo.models import Subject
 
 class Department(models.Model):
     department_name = models.CharField(unique=True, max_length=150)
-    
+
     def __str__(self):
         return self.department_name
+
 
 class Professor(models.Model):
     department = models.ForeignKey(Department, on_delete=models.CASCADE)
