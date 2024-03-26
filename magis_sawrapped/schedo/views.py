2a995849226874605d42299c3a98e0276a7f3179
Almira Redoble <almira.redoble@obf.ateneo.edu>
2024-03-26 20:57:37 +0800

    Implementing responsive table

--- magis_sawrapped/schedo/views.py
+++ magis_sawrapped/schedo/views.py
@@ -1,18 +1,20 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 from django_tables2 import SingleTableMixin
+from django_filters.views import FilterView
 
 from .models import UserSchedule, UserTable
 from .tables import UScheduleHTMxTable
+
 
 def index(request):
     table = UserTable.objects.all()
     return render(request, 'index.html', {'table': table})
 
-class UScheduleHTMxTableView(SingleTableMixin):
+class UScheduleHTMxTableView(SingleTableMixin, FilterView):
     table_class = UScheduleHTMxTable
     queryset = UserSchedule.objects.all()
+    model = UserSchedule
 
     def get_template_names(self):
         if self.request.htmx:
