d837aeb5ada198183918167ae603ca8d050cf4c5
Washington99 <118784933+Washington99@users.noreply.github.com>
2024-04-19 14:12:03 +0800

    Added a details view for UserTable
    Home link for the schedo now has links to all other tables that when clicked redirects to a page displaying the info related to the table

--- magis_sawrapped/schedo/urls.py
+++ magis_sawrapped/schedo/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 
-from .views import UScheduleHTMxTableView
+from .views import *
 
 urlpatterns = [
     path('', UScheduleHTMxTableView.as_view(), name='index'),
+    path('<int:pk>/details/', UserTableDetailView.as_view(), name='userTable-details'),
+    path('add/', UserTableCreateView.as_view(), name='userTable-create'),
+    path('<int:pk>/edit/', UserTableUpdateView.as_view(), name='userTable-update'),
+    # path('index_card', index_card_view, name='index_card'),
 ]
 
 app_name = "schedo"
--- magis_sawrapped/schedo/views.py
+++ magis_sawrapped/schedo/views.py
@@ -1,20 +1,42 @@
 from django.shortcuts import render
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView
 
 from django_tables2 import SingleTableMixin
 from django_filters.views import FilterView
 
 from .models import UserSchedule, UserTable
 from .tables import UScheduleHTMxTable
+from .forms import UserTableForm
 
 
 def index(request):
-    table = UserTable.objects.all()
-    return render(request, 'index.html', {'table': table})
+    context = {}
+    context['UserTables'] = UserTable.objects.all()
+    return render(request, 'product_table_htmx.html', context)
+
+class UserTableDetailView(DetailView):
+    context = {}
+    model = UserTable
+    context['UserSchedules'] = UserSchedule.objects.all()
+    template_name = "Schedo/schedo_details.html"
+
+class UserTableCreateView(CreateView):
+    
+    model = UserTable
+    
+    template_name = "schedo.html"
+    
+class UserTableUpdateView(UpdateView):
+    model = UserTable
+    fields = '__all__'
+    template_name = 'schedo.html'
+
 
 class UScheduleHTMxTableView(SingleTableMixin, FilterView):
     table_class = UScheduleHTMxTable
-    queryset = UserSchedule.objects.all()
-    model = UserSchedule
+    queryset = UserTable.objects.all()
+    model = UserTable
 
     def get_template_names(self):
         if self.request.htmx:
@@ -23,3 +45,19 @@ class UScheduleHTMxTableView(SingleTableMixin, FilterView):
             template_name = "product_table_htmx.html"
 
         return template_name
+
+# class UserTableListView(ListView):
+#     model = UserTable
+#     def get_context_data(self, **kwargs):
+
+#         context = super().get_context_data(**kwargs)
+#         context['form'] = UserTableForm()
+#         return context
+#     def post(self, request, *args, **kwargs):
+
+#         form = UserTableForm(request.POST) # Create a Form object from the POST values
+#         if form.is_valid():
+#             return self.get(request, *args, **kwargs)
+
+#         else:
+#             return render(request, self.template_name, {'form': form})
