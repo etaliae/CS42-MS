2dfab696e1d60c17a42c284bf8c97b93c48ca4e6
Washington99 <118784933+Washington99@users.noreply.github.com>
2024-04-27 02:31:16 +0800

    UserSchedule
    Added detail, edit and add views for UserSchedules

--- magis_sawrapped/schedo/forms.py
+++ magis_sawrapped/schedo/forms.py
@@ -1,7 +1,12 @@
-from .models import UserTable
+from .models import UserTable, UserSchedule
 from django import forms
 
 class UserTableForm(forms.ModelForm):
     class Meta:
         model = UserTable
+        fields = "__all__"
+        
+class UserScheduleForm(forms.ModelForm):
+    class Meta:
+        model = UserSchedule
         fields = "__all__"
--- magis_sawrapped/schedo/urls.py
+++ magis_sawrapped/schedo/urls.py
@@ -5,7 +5,10 @@ from .views import *
 urlpatterns = [
     path('', UScheduleHTMxTableView.as_view(), name='index'),
     path('<int:pk>/details/', UserTableDetailView.as_view(), name='userTable-details'),
+    path('addSchedule/', UserScheduleAdd.as_view(), name='userSchedule-add'),
+    path('<int:pk>/schedule/', UserScheduleEditView.as_view(), name='userSchedule-edit'),
     path('<int:pk>/edit/', UserTableEdit.as_view(), name='userTable-edit'),
+    path('', UserTableDelete.as_view(), name='userTable-delete'),
     path('add/', UserTableAdd.as_view(), name='userTable-add'),
     path('landing/', landing, name="landing"),
     path('courses/', CourseHTMxTableView.as_view(), name="courses"),
